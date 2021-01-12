56b15f495f54325e4077ca4cab01c73cbffc0024
Martin STEFFEN <msteffen.externe@bedrockstreaming.com>
2021-01-12 12:33:47 +0100

    new dimenssion following hero me gen 5 mod

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -987,7 +987,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 40, 10, -1.48 }
+#define NOZZLE_TO_PROBE_OFFSET { 55, -5, -1.48 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1158,7 +1158,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define Y_BED_SIZE 215
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
